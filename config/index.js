83e9c02940701550676a6571fe116d5ec1797d5f
jainalka <77779628+jainalka@users.noreply.github.com>
2021-03-05 18:11:24 +0530

    Add files via upload

---
+++ config/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+    db_uri : process.env.MONGO_URI || "mongodb+srv://alka_ajmera:ajmera22@cluster0.gxry6.mongodb.net/restapi?retryWrites=true"
+}
---
+++ controller/user.controller.js
@@ -0,0 +1,102 @@
+const express = require('express');
+const assert = require('assert');
+const UserModel =require("../model").UserSchema;
+const userModel = require('../model/user.model');
+
+module.exports = {
+    
+        index: (req,res)=> {
+            res.render("index.ejs");
+
+        },
+        details: (req,res)=> {
+            res.render("details.ejs");
+
+        },
+        contact: (req,res)=> {
+            res.render("contact.ejs");
+        },
+
+        api : (req,res)=> {
+            res.render("path-not-found.ejs");
+        },
+        createUser: (req,res)=> {
+           
+            let data = new UserModel(req.body);
+            console.log('new user data', data);
+            data.save().then(result=>{
+                res.status(200).json({code:200, message: "successfully saved new user"});
+            }).catch(err=> {
+                res.status(200).json({code: 304, message: "Unable to save user"})
+            });
+        },
+        readUser: (req,res)=> {
+            UserModel.find({},(err,result)=> {
+                if(err){
+                    assert.deepStrictEqual(err,null);
+                    res.status(200).json({code:304, message: "No data found"});
+                } else {
+                    res.json(result);
+                }
+
+             });
+    },
+        readSingleUser: (req,res)=> {
+            // read user id from route path
+            let id = req.params.id;
+            UserModel.findById({_id: id},(err,result)=> {
+                if(err){
+                    assert.deepStrictEqual(err,null);
+                    res.status(200).json({code:304, message: "No user found"});
+                } else {
+                    if(result === null){
+                        res.status(200).json({code:304,message:"No data found"});
+                    }else {
+                    res.json(result);
+                }
+            }
+            });
+        },
+        updateUser: (req,res)=> {
+            //update
+            let id=req.params.id;
+            let data = new UserModel(req.body);
+
+            userModel.findByIdAndUpdate(
+                {_id: id},
+                {
+                    name: data.name,
+                    email: data.email,
+                    mobile: data.mobile,
+                    username: data.username,
+                    password: data.password,
+                    created: new Date().toLocaleString()
+                },
+                {upsert: true},
+                (err,result)=>{
+                    if(err){
+                    assert.deepStrictEqual(err,null);
+                    res.status(200).json({code:304, message: "unable to update"});
+                } else {
+                    res.status(200).json({code:200,message: "successfully updated "});
+
+                }
+            }
+            )
+
+
+        },
+        deleteUser: (req,res)=> {
+            // read id from route path
+            let id =req.params.id;
+            UserModel.findByIdAndDelete({_id: id}, (err,result)=>{
+                if(err){
+                    assert.deepStrictEqual(err,null);
+                    res.status(200).json({code:304, message: " user not found"});
+                } else {
+                    
+                    res.status(200).json({code:200, message: " user successfully deleted"});
+                }
+            });
+        }
+    };
